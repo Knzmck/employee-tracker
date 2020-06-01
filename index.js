eefc9b18573f11a918d42c3c309c81cecbc3761c
Mackenzie Schutz <mshoots64@gmail.com>
2020-06-01 14:48:42 -0500

    add inquirer prompt

--- index.js
+++ index.js
@@ -1,16 +1,86 @@
-const util = require("util");
-const mysql = require("mysql");
+// Prompt users:
+// view all employee
+// view employees by department
+// View employees by manager
+// Add employee
+// Remove employee
+// update employee role
+// update employee manager 
+// View all roles
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password", 
-    database: "employees"
+//  id, first name, last name, title, department, salary, manager for each employee
 
-});
+// depending on choice have a switch case scenario 
 
-connection.connect();
 
-connection.query = util.promisify(connection.query);
+const inquirer = require("inquirer");
+const db = require("./db");
 
-module.exports = connection
+async function mainPrompt() {
+    const { userChoice } = await prompt([
+        {
+            type: "list",
+            name: "first",
+            message: "Choose an action:",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "View All Employees By Department",
+                    value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+                },
+                {
+                    name: "View All Employees By Manager",
+                    value: "VIEW_EMPLOYEES_BY_MANAGER"
+                },
+                {
+                    name: "Add Employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                {
+                    name: "Remove Employee",
+                    value: "REMOVE_EMPLOYEE"
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "UPDATE_EMPLOYEE_ROLE"
+                },
+                {
+                    name: "Update Employee Manager",
+                    value: "UPDATE_EMPLOYEE_MANAGER"
+                },
+                {
+                    name: "View All Roles",
+                    value: "VIEW_ROLES"
+                },
+                {
+                    name: "Add Role",
+                    value: "ADD_ROLE"
+                },
+                {
+                    name: "Remove Role",
+                    value: "REMOVE_ROLE"
+                },
+                {
+                    name: "View All Departments",
+                    value: "VIEW_DEPARTMENTS"
+                },
+                {
+                    name: "Add Department",
+                    value: "ADD_DEPARTMENT"
+                },
+                {
+                    name: "Remove Department",
+                    value: "REMOVE_DEPARTMENT"
+                },
+                {
+                    name: "Quit",
+                    value: "QUIT"
+                }
+            ]
+        }
+    ])
+};
+
