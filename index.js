0a4c6e5a778ffc9ca36139184644dbd74b4a6eb6
Mackenzie Schutz <mshoots64@gmail.com>
2020-06-01 14:24:06 -0500

    install dependencies. create connection to server

---
+++ index.js
@@ -0,0 +1,16 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password", 
+    database: "employees"
+
+});
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection
