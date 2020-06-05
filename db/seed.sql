cffc86f26e924e6122718fd60a347999fc8f55f7
Mackenzie Schutz <mshoots64@gmail.com>
2020-06-05 11:28:37 -0500

    add new values to table

--- db/seed.sql
+++ db/seed.sql
@@ -6,11 +6,32 @@
 
 USE employees_DB
 
-INSERT INTO departments (department_name)
-VALUES ("HR"), ("Accounting"), ("Sales"), ("Legal"), ("IT")
+INSERT INTO departments 
+    (department_name)
+VALUES  
+("Upper Management"), 
+("HR"), 
+("Accounting"), 
+("Sales"), 
+("Legal"), 
+("IT"),
 
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Director of HR", 50000.00, 1), ("Director of Accounting", 50000.00, 2), ("Director of Sales", 50000.00, 3), ("Director of Legal", 50000.00, 4), ("Director of IT", 50000.00, 5)
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO roles 
+    (title, salary, department_id)
 VALUES 
+("Manager", 42000.00, 1),
+("Director of HR", 50000.00, 2), 
+("Director of Accounting", 50000.00, 3), 
+("Director of Sales", 50000.00, 4), 
+("Director of Legal", 120000.00, 5), 
+("Director of Customer Service", 50000.00, 6)
+
+INSERT INTO employee 
+    (first_name, last_name, role_id, manager_id)
+VALUES 
+("Michael", "Scott", 1, 1), 
+("Toby", "Flenderson", 2, 2), 
+("Angela", "Martin", 3, 2), 
+("Dwight", "Schrute", 4, 2), 
+("Dianne", "Kelly", 4 2), 
+("Kelly", "Kapoor", 6, 2)
